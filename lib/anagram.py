f086259fefb62b189a8d89d7dde7fe9eda30a002
Ej Myles <ejelymyles@gmail.com>
2023-10-16 00:01:51 -0700

    finish lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for candidate in words:
+            if sorted(candidate.lower()) == sorted(self.word.lower()) and candidate.lower() != self.word.lower():
+                anagrams.append(candidate)
+        return anagrams
